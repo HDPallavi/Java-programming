323e80b1ef1218b976fc99c4ac15c252364e5c57
pallavi <pallavi.hd@reconnectenergy.com>
2025-01-04 22:58:21 +0530

    set

---
+++ src/test/java/collection/Hashset.java
@@ -0,0 +1,16 @@
+package collection;
+
+import java.util.HashSet;
+
+public class Hashset 
+{
+	public static void main(String[] args) 
+	{
+		HashSet<String> h=new HashSet<String> ();
+		h.add("welcome");
+		h.add("abc");
+		h.add("1");
+		System.out.println(h);
+	}
+
+}
